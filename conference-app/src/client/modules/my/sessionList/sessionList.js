d896099ab680ec5f8c71fab24aea4fec957be151
Jack Chou <jcacct@protonmail.ch>
2020-05-18 17:14:44 -0700

    Revert "Create the Session Details Web Component"

--- conference-app/src/client/modules/my/sessionList/sessionList.js
+++ conference-app/src/client/modules/my/sessionList/sessionList.js
@@ -13,11 +13,4 @@ export default class SessionList extends LightningElement {
       session => session.name.toLowerCase().includes(searchKey)
     );
   }
-  handleSessionClick(event) {
-    const index = event.currentTarget.dataset.index;
-    const navigateEvent = new CustomEvent('navigate', {
-      detail: this.sessions[index].id
-    });
-    this.dispatchEvent(navigateEvent);
-  }
 }
