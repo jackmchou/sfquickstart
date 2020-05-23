54fbe88e2af5934649c6021da933a0973fe1bb8a
Jack Chou <jcacct@protonmail.ch>
2020-05-23 11:20:55 -0700

    Added mock data to sessiondetails js

--- lightning-conference/force-app/main/default/lwc/my/sessionDetails/sessionDetails.js
+++ lightning-conference/force-app/main/default/lwc/my/sessionDetails/sessionDetails.js
@@ -1,5 +1,4 @@
 import { LightningElement, api } from 'lwc';
-import { getSession } from 'data/sessionService';
 export default class sessionDetails extends LightningElement {
   session;
   @api
@@ -10,7 +9,29 @@ export default class sessionDetails extends LightningElement {
   // the component is automatically rerendered.
   set sessionId(id) {
     this._sessionId = id;
-    this.session = getSession(id);
+    this.session = {
+      id: '1',
+      name: 'Mock session',
+      dateTime: '2099-01-01 00:00:00',
+      room: 'Mock room',
+      description: "Mock description",
+      speakers: [
+        {
+          id: '1',
+          name: 'Mock speaker 1',
+          bio: 'Bio for mock speaker 1',
+          email: 'mock1@trailhead.com',
+          pictureUrl: 'https://developer.salesforce.com/files/js-dev/speaker-images/john_doe.jpg'
+        },
+        {
+          id: '2',
+          name: 'Mock speaker 2',
+          bio: 'Bio for mock speaker 2',
+          email: 'mock2@trailhead.com',
+          pictureUrl: 'https://developer.salesforce.com/files/js-dev/speaker-images/laetitia_arevik.jpg'
+        }
+      ]
+    };
   }
   get sessionId() {
     return this._sessionId;
