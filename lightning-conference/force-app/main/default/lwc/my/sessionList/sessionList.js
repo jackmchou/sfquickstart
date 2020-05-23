79fbf2d111ef25e38448a815c9631d3356810065
Jack Chou <jcacct@protonmail.ch>
2020-05-23 11:19:18 -0700

    Added mock data for connected callback method for testing

--- lightning-conference/force-app/main/default/lwc/my/sessionList/sessionList.js
+++ lightning-conference/force-app/main/default/lwc/my/sessionList/sessionList.js
@@ -1,11 +1,32 @@
 import { LightningElement } from 'lwc';
-import { getSessions } from 'data/sessionService';
 export default class SessionList extends LightningElement {
   sessions = [];
   connectedCallback() {
-    getSessions().then(result => {
-      this.sessions = this.allSessions = result;
-    });
+    this.sessions = this.allSessions = [
+      {
+        id: '1',
+        name: 'Mock session',
+        dateTime: '2099-01-01 00:00:00',
+        room: 'Mock room',
+        description: "Mock description",
+        speakers: [
+          {
+            id: '1',
+            name: 'Mock speaker 1',
+            bio: 'Bio for mock speaker 1',
+            email: 'mock1@trailhead.com',
+            pictureUrl: 'https://developer.salesforce.com/files/js-dev/speaker-images/john_doe.jpg'
+          },
+          {
+            id: '2',
+            name: 'Mock speaker 2',
+            bio: 'Bio for mock speaker 2',
+            email: 'mock2@trailhead.com',
+            pictureUrl: 'https://developer.salesforce.com/files/js-dev/speaker-images/laetitia_arevik.jpg'
+          }
+        ]
+      }
+    ];
   }
   handleSearchKeyInput(event) {
     const searchKey = event.target.value.toLowerCase();
